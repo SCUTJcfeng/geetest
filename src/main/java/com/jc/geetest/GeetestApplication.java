b983fdd2b9857c7e9720e5cdefd50acb0ca6f468
fengjianchao <jcfeng2013@gmail.com>
2019-09-13 09:44:40 +0800

    init commit

---
+++ src/main/java/com/jc/geetest/GeetestApplication.java
@@ -0,0 +1,13 @@
+package com.jc.geetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeetestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeetestApplication.class, args);
+    }
+
+}
